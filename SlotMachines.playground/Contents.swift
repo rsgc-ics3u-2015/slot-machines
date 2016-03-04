f453c18dc23fa9dd374770ac4034aef11ae8e0e0
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-04 17:46:17 -0500

    Recorded my initial ideas about the problem

---
+++ SlotMachines.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Russell Gordon
+//
+// "Slot machines"
+
+/*
+
+Basically, when a starting number of coins...
+
+... play three machines, each after the other, a, b, then c.  
+
+Then back to a again, and so on.
+
+Machine A pays 30 coins every 35 turns
+Machine B pays 60 coins every 100 turns
+Machine C 9 coins every 10 turns
+
+I need to keep track of the number of plays before coins run out (reach 0).
+
+*/
